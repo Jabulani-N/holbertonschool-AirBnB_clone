1f966ca27e2e5629333844f1c08b003335e0cac6
Jabulani-N <jabu.94@gmail.com>
2023-06-09 14:13:50 -0700

    updated_test_base_model-self-id_assignment

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -5,3 +5,23 @@ This module exclusively tests the BaseModel class
 
 import unittest
 from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """tests class for class BaseModel.
+    define a test, set up whatever you need between the
+    def and self.assert.
+
+    a proper setup() function will accelerate class creation
+
+    you'll probably want to mock a known datetime value to put into this
+    """
+
+    #first test cluster: public attributes
+    # self.assertEqual(thing, what_thing_should_equal_to_pass_test)
+    def test_instanciation(self):
+        newbase1 = BaseModel()
+        self.assertEqual(type(newbase1.id), str)
+
+
+if __name__ == '__main__':
+    unittest.main
