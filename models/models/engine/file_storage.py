f5ea4658bcd4a18a86f090f45166c0889d233227
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-06-12 21:48:49 +0000

    1

--- models/models/engine/file_storage.py
+++ models/models/engine/file_storage.py
@@ -17,5 +17,22 @@ class FileStorage:
         FileStorage.__objects[key] = obj
 
     def save(self):
+        new_dict = {}
+        for key in FileStorage. __objects:
+            new_dict[key] = FileStorage.__objects[key].to_dict()
+        with open(FileStorage.__file_path, mode='w', encoding='utf-8') as f:
+            json.dump(new_dict, f)
+
+    def reload(self):
         try:
-            with
+            with open(FileStorage.__file_path, mode='r', encoding='utf-8') as f:
+                json_dict = json.load(f)
+            for key in json_dict:
+                class_name, obj_id = key.split('.')
+                module = __import__('models.' + class_name.lower(), fromlist=
+                [class_name])
+                obj_cls = getattr(module, class_name)
+                obj = obj_cls(**json_dict[key])
+                FileStorage.__objects[key] = obj
+            except FileNotFoundError:
+                pass
